886346b17c65429633fcf315c9a700bc036511f2
James Harding <jamesvitaly1993@gmail.com>
2019-08-26 17:43:28 +0100

    Updated navbar component file name

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from 'styled-components';
-import Navbar from "./Navbar"
+import Navbar from "./navbar"
 import "./layout.css"
 
 
